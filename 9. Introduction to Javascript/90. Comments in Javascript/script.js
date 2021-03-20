086479722d0c9b65ccdf9506661c206e7a18d4eb
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2021-03-20 21:23:14 +0530

    Add files via upload

---
+++ 9. Introduction to Javascript/90. Comments in Javascript/script.js
@@ -0,0 +1,10 @@
+// This is my function and its name is myFunction()
+function myFunction(){
+    document.getElementById("demo").innerHTML = "This is new paragraph ..."
+}
+
+/*
+Multi 
+line 
+Comments
+*/
