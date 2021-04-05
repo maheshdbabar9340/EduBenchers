aaae9682525485eef9406a52509bd4c5793f624c
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2021-04-05 10:19:48 +0530

    new started for php

---
+++ 14. PHP/167. PHP First Page with Language Basics.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo " My First PHP Script !!"
+?>
+
+
+</body>
+</html>
---
+++ 14. PHP/168. PHP Variables and Constants.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$txt = "Hello EduBenchers!!";
+$x = 5;
+$y = 10;
+
+$age = 15;
+$AGE = 25;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+
+define("Greetings", "Welcome to Edubenchers!!");
+echo Greetings;
+?>
+
+</body>
+</html>
