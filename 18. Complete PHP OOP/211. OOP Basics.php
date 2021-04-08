4e0480089a8c72fd1faa8d5f45385141289fdbbb
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2021-04-08 15:28:21 +0530

    starting OOPs

---
+++ 18. Complete PHP OOP/211. OOP Basics.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Advanced  PHP</h1>
+
+<?php
+echo "What is OOP?";
+echo "<br>";
+echo "Advantages of Object-oriented programming over Procedural Language";
+?>
+
+</body>
+</html>
